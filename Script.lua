c889ca58a43ee44ffb90bd954b56e6641a80330d
fleekx3 <110290161+fleekx3@users.noreply.github.com>
2025-05-09 15:42:52 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,9 +1,8 @@
 repeat task.wait() until game:IsLoaded()
-function Maru()
-repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+function Maru()
     _G.Team = "Pirate" -- Marine / Pirate
     _G.MainSettings = {
         ["EnabledHOP"] = true, -- เปิด HOP ( มันไม่มีอยู่ละใส่มาเท่ๆ )
